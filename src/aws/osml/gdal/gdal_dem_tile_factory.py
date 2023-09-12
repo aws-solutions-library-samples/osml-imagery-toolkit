a426f2098febaa547e819db19aca1ad91c29902c
edparris <edparris@gmail.com>
2023-09-12 10:43:01 -0400

    feat: add HAE and DEM projections to SICD sensor model

--- src/aws/osml/gdal/gdal_dem_tile_factory.py
+++ src/aws/osml/gdal/gdal_dem_tile_factory.py
@@ -1,9 +1,16 @@
 import logging
 from typing import Any, Optional, Tuple
 
+import numpy as np
 from osgeo import gdal
 
-from aws.osml.photogrammetry import DigitalElevationModelTileFactory, GDALAffineSensorModel
+from aws.osml.photogrammetry import (
+    DigitalElevationModelTileFactory,
+    ElevationRegionSummary,
+    GDALAffineSensorModel,
+    ImageCoordinate,
+    geodetic_to_geocentric,
+)
 
 
 class GDALDigitalElevationModelTileFactory(DigitalElevationModelTileFactory):
@@ -24,7 +31,9 @@ class GDALDigitalElevationModelTileFactory(DigitalElevationModelTileFactory):
         super().__init__()
         self.tile_directory = tile_directory
 
-    def get_tile(self, tile_path: str) -> Tuple[Optional[Any], Optional[GDALAffineSensorModel]]:
+    def get_tile(
+        self, tile_path: str
+    ) -> Tuple[Optional[Any], Optional[GDALAffineSensorModel], Optional[ElevationRegionSummary]]:
         """
         Retrieve a numpy array of elevation values and a sensor model.
 
@@ -32,7 +41,7 @@ class GDALDigitalElevationModelTileFactory(DigitalElevationModelTileFactory):
 
         :param tile_path: the location of the tile to load
 
-        :return: an array of elevation values and a sensor model or (None, None)
+        :return: an array of elevation values, a sensor model, and a summary or (None, None, None)
         """
         tile_location = f"{self.tile_directory}/{tile_path}"
         tile_location = tile_location.replace("s3:/", "/vsis3", 1)
@@ -43,15 +52,31 @@ class GDALDigitalElevationModelTileFactory(DigitalElevationModelTileFactory):
         # information isn't available.
         if not ds:
             logging.debug(f"No DEM tile available for {tile_path}. Checked {tile_location}")
-            return None, None
+            return None, None, None
 
         # If the raster exists but doesn't have a geo transform then it is likely invalid input data.
         geo_transform = ds.GetGeoTransform(can_return_null=True)
         if not geo_transform:
             logging.warning(f"DEM tile does not have geo transform metadata and can't be used: {tile_location}")
-            return None, None
+            return None, None, None
 
-        band_as_array = ds.GetRasterBand(1).ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
+        raster_band = ds.GetRasterBand(1)
+        band_as_array = raster_band.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
+        height, width = band_as_array.shape
         sensor_model = GDALAffineSensorModel(geo_transform)
 
-        return band_as_array, sensor_model
+        # Compute the distance in meters from the upper left to the lower right corner. Divide that by the distance
+        # in pixels to get an approximate pixel size in meters
+        ul_corner_ecf = geodetic_to_geocentric(sensor_model.image_to_world(ImageCoordinate([0, 0]))).coordinate
+        lr_corner_ecf = geodetic_to_geocentric(sensor_model.image_to_world(ImageCoordinate([width, height]))).coordinate
+        post_spacing = np.linalg.norm(ul_corner_ecf - lr_corner_ecf) / np.sqrt(width * width + height * height)
+
+        stats = raster_band.GetStatistics(True, True)
+        summary = ElevationRegionSummary(
+            min_elevation=stats[0],
+            max_elevation=stats[1],
+            no_data_value=raster_band.GetNoDataValue(),
+            post_spacing=post_spacing,
+        )
+
+        return band_as_array, sensor_model, summary
