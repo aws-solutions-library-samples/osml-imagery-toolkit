340d5036977f3f021fb38ec69774a855a1c208af
edparris <edparris@gmail.com>
2024-03-22 14:05:20 -0400

    docs: add badges and fix broken links in package description

--- src/aws/osml/features/__init__.py
+++ src/aws/osml/features/__init__.py
@@ -1,16 +1,24 @@
 #  Copyright 2023-2024 Amazon.com, Inc. or its affiliates.
 
+# Telling flake8 to not flag errors in this file. It is normal that these classes are imported but not used in an
+# __init__.py file.
+# flake8: noqa
+"""
+The features package contains classes that assist with working with geospatial features derived from imagery.
+
+-------------------------
+
+APIs
+****
+"""
+
 from .feature_index import Feature2DSpatialIndex, STRFeature2DSpatialIndex
 from .geolocation import Geolocator
 from .imaged_feature_property_accessor import ImagedFeaturePropertyAccessor
 
-"""
-The features package contains classes that assist with working with geospatial features derived from imagery.
-"""
-
 __all__ = [
-    "Feature2DSpatialIndex",
     "Geolocator",
     "ImagedFeaturePropertyAccessor",
+    "Feature2DSpatialIndex",
     "STRFeature2DSpatialIndex",
 ]
